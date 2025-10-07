aa5674d2bb54e947e0ce02facef213a3a0ef91c0
LivinkumarS <001liv100@ggmail.com>
2025-10-07 13:38:25 +0530

    Introduction

---
+++ Introduction.py
@@ -0,0 +1,12 @@
+# Print(99)
+# print(199)
+
+'''this paragraph is to show you how to create multiple line comments. 
+I hope this will be helpfull for you.'''
+
+# print(299)
+# print(399)
+# print("Livinkumar")
+
+a=10
+print(a)
