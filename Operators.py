7263763d194b90829141b1410e2a91609731f6be
LivinkumarS <001liv100@ggmail.com>
2025-10-09 11:18:46 +0530

    Operators

--- Operators.py
+++ Operators.py
@@ -1,5 +1,3 @@
-a=10
-b=20
 # a=b/a
 
 # print(45//7)
@@ -8,4 +6,44 @@ b=20
 
 # print(2**0)
 
-print(2+3-6/3*20)
+# print(2+3-6/3*20)
+
+# print(a==b)
+# print(a!=b)
+# print(6<8)
+# print(-6>-8)
+# a=10
+# b=20
+# print(a<=15)
+# print(a+5>=15)
+
+# a=56
+# a+=7 # a=a+7 --> 63
+# a-=33
+# a*=(1/3)
+# a/=5 #2
+# a//=3
+# a+=8
+# a%=3
+# a**=4
+
+# print(a)
+
+# b1=True 
+# b2=False 
+
+# print(b1 and b2)
+# print(not b1 or b2)
+
+# print(not b1)
+
+# a=2
+# b=7
+
+# print((a>6) and (a+b**6<b**7 or not b-a+7<7**8))
+
+# print(9&11)
+
+# print(5|20)
+# print(~5)
+print(5<<2)
