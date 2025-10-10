af4f8f221f7fa87ddef15df21faabc81096d261b
LivinkumarS <001liv100@ggmail.com>
2025-10-10 11:19:17 +0530

    Control Statement

---
+++ ControlFlow.py
@@ -0,0 +1,74 @@
+# inp=int(input("Enter Your Age: "))
+
+# if inp>=18:
+#     print("Can vote!")
+# else:
+#     print("Cannot vote!")
+
+
+# if a**2>b:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+
+
+# print("Final step")
+# a=17
+# b=99
+
+# if a<b:
+#     print("From If Block!")
+# else:
+#     print("From Else Block!")
+
+#inp... >=10 <15 large. >15 very large. <10 less
+
+# inp=int(input("Enter Your Number: "))
+
+# if inp>=10 and inp<15:
+#     print("Large")
+# elif inp>=15:
+#     print("Very Large")
+# else:
+#     print("Less")
+
+# int=1
+
+# if int>10: #false
+#     print("Hello")
+# elif int>-10: #true
+#     print("Hi")
+# elif int>-8: #true
+#     print("Vanakam")
+# else:
+#     print("Good morning!")
+
+
+# marks = int(input("Enter your marks: "))
+
+# if marks > 90:
+#     print("O")
+# elif marks > 80 and marks <= 90:
+#     print("A+")
+# elif marks > 70 and marks <= 80:
+#     print("A")
+# elif marks > 60 and marks <= 70:
+#     print("B")
+# elif marks > 50 and marks <= 60:
+#     print("C")
+# else:
+#     print("Fail")
+
+a=int(input("Number: "))
+
+if(a>15):
+    if(a<=20):
+        print("Large")
+    else:
+        print("Very Large")
+else:
+    if(a>=10):
+        print("Small")
+    else:
+        print("Very Small")
+        
