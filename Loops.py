9defb20112908ec2db8f844625ca7946ef8ea663
LivinkumarS <001liv100@ggmail.com>
2025-10-13 11:19:36 +0530

    Loops - Basics

---
+++ Loops.py
@@ -0,0 +1,30 @@
+# a=0
+
+# while a<5: 
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print()
+#     print("---------------------")
+#     print()
+#     a-=1
+    
+# a --> 0,-1,-2,-100......
+# cond ---> True
+# no of execution---> 1,2,3,4,5
+
+# str1="Hello world"
+# print(str1[10])
+
+# str1="123456789"
+# print(len(str1))
+
+# for i in range(100):
+#     print(i)
+# for i in range(3,10,3):
+#     print(i)
+
+# 1-- k
+# 2-- a
+# 3-- n
+# 4-- t
