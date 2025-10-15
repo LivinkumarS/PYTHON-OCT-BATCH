669bdbcb6f03f10bc3933617c0ea7076ef3e29e5
LivinkumarS <001liv100@ggmail.com>
2025-10-15 11:22:59 +0530

    Functions and Scopes

---
+++ Function.py
@@ -0,0 +1,60 @@
+# def printSteps():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     return [34,34,34,34]
+
+# a=printSteps()
+# print(a)
+
+# def showPro(a,b,c):
+#     print(a*b*c)
+
+# showPro(4,5,6)
+# showPro(1,2,3)
+
+# showPro(b=4,c=4,a=1)
+
+# def showSum(a,b=10):
+#     print(a+b)
+
+# showSum(5,5)
+
+# def showSum(*num):
+#     sum=0
+#     for i in num:
+#         sum +=i
+#     print(sum)
+
+# showSum(2,34,54,3,6,4554)
+
+# def sayHello(name):
+#     print("Good Morning "+name+"!")
+
+# sayHello("Livin")
+
+# def printPro(a,b):
+#     print(a*b)
+
+
+
+# def findPro(a,b):
+#     return a*b 
+
+# print(findPro(findPro(2,3),findPro(findPro(3,4),4)))
+
+
+# a=10
+
+def dummy():
+    a=15
+    print("a's value inside the function: ",a)
+
+def dummy2():
+    print("a's value inside the function2: ",a)
+
+
+    
+dummy()
+dummy2()
+# print("a's value outside the function: ",a)
